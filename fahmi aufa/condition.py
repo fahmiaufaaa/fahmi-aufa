69c9d9335050f4051698687aea3c1b3c0b290844
fahmiaufaaa <141303582+fahmiaufaaa@users.noreply.github.com>
2023-08-10 13:31:49 +0700

    Add files via upload

---
+++ fahmi aufa/condition.py
@@ -0,0 +1,23 @@
+nilai i= int(input("masukkan nilai mahasiswa"))
+ekstrakulkuler = input("apakah mahasiswa aktif")
+if nilai >=n 90:
+    if ekstrakulikuler == "ya":
+        nilai_akhir = "A"
+        else:
+        nilai_akhir = "A+"
+else:
+        nilai_akhir = "A-"
+
+elif nilai >= 80 and nilai <90:
+    if ekstrakulikuler == "ya":
+        nilai_akhir = "B"
+        else:
+        nilai_akhir = "B+"
+        else:
+        nilai_akhir = "B-"
+            if ekstrakulikuler == "ya":
+        nilai_akhir = "C"
+        else:
+        nilai_akhir = "c+"
+        else:
+        nilai_akhir = "C-"
---
+++ fahmi aufa/loop.py
@@ -0,0 +1,12 @@
+# while
+x = 1
+while (x<6):
+    print(x);
+    x+=1;
+
+#for
+i = 1
+
+for i in range (6):
+    print(x);
+    i+=1;
